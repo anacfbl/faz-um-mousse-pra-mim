cba67c7d68b7b4a6f72b594230ac710539e856cb
Ana Clara <anaclarablopes@gmail.com>
2020-09-02 10:01:12 -0300

    tremida ok

--- Faz um Mousse pra mim?/Faz um Mousse pra mim?/MaracujaViewController.swift
+++ Faz um Mousse pra mim?/Faz um Mousse pra mim?/MaracujaViewController.swift
@@ -22,102 +22,80 @@ class MaracujaViewController: UIViewController, UIGestureRecognizerDelegate {
     
     var maracuja = UIImage.init (named: "maracuja-1")
     
-    @IBAction func gestureMaracuja(_ sender: Any) {
-        buttonProximo.isHidden = false
-        imageMaracuja.image = maracuja
-        semente.isHidden = false
-        
-    } // fecha tapGesture
+    //    @IBAction func gestureMaracuja(_ sender: Any) {
+    //        buttonProximo.isHidden = false
+    //        imageMaracuja.image = maracuja
+    //        semente.isHidden = false
+    //
+    //    } // fecha tapGesture
     
-    
-    
-//     tocou fora do maracuja:
-        override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-    
-            super.touchesBegan(touches, with: event)
-    
-            let touch = touches.first
-            
-            guard let location = touch?.location(in: self.view) else { return }
-            if !viewMaracuja.frame.contains(location) {
-               
-                let animation = CABasicAnimation(keyPath: "position")
-                animation.duration = 0.07
-                animation.repeatCount = 4
-                animation.autoreverses = true
-                animation.fromValue = NSValue(cgPoint: CGPoint(x: viewMaracuja.center.x - 8, y: viewMaracuja.center.y))
-                animation.toValue = NSValue(cgPoint: CGPoint(x: viewMaracuja.center.x + 8, y: viewMaracuja.center.y))
-    
-                viewMaracuja.layer.add(animation, forKey: "position")
-            }
-        }
     
     
     
     // DRAG AND DROP
-
-    
-        var initialCenter = CGPoint()
-    
-        @IBAction func dragDrop(_ sender: UIPanGestureRecognizer) {
     
     
-            guard sender.view != nil else {return}
+    var initialCenter = CGPoint()
     
-            let gesture = sender.view!
-            let translation = sender.translation(in: gesture.superview)
+    @IBAction func dragDrop(_ sender: UIPanGestureRecognizer) {
+        
+        
+        guard sender.view != nil else {return}
+        
+        let gesture = sender.view!
+        let translation = sender.translation(in: gesture.superview)
+        
+        
+        if sender.state == .began {
+            self.initialCenter = gesture.center }
+        
+        
+        if sender.state == .cancelled {
+            let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+            gesture.center = newCenter }
+            
+            
+        else {
+            gesture.center = initialCenter }
+        
+        
+    } //
     
     
-            if sender.state == .began {
-                self.initialCenter = gesture.center }
-    
-    
-            if sender.state == .cancelled {
-                let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
-                gesture.center = newCenter }
-    
-    
-            else {
-                gesture.center = initialCenter }
-    
-    
-        } //
-    
-    
-//    var initialCenter = CGPoint()
-//    var movedImage = UIImageView ()
-//    var dropArea = UIView ()
-//
-//    @IBAction func dragDrop(_ sender: Any) {
-//
-//        func drag (sender: UIPanGestureRecognizer) {
-//
-//            //               self.view.bringSubviewToFront(semente)
-//
-//            let translation = sender.translation(in: self.view)
-//
-//            //               if let view = sender.view {
-//            //                   view.center = CGPoint(x:view.center.x + translation.x,
-//            //                                         y:view.center.y + translation.y)
-//
-//            //                   if sender.state == UIGestureRecognizer.State.ended {
-//
-//            let here = sender.location(in: self.view)
-//            let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
-//
-//            if (viewMaracuja.frame.contains(here)) {
-//                semente.center = newCenter }
-//
-//            else {
-//                semente.center = initialCenter
-//                //                       }
-//
-//            }
-//
-//            sender.setTranslation(CGPoint.zero, in: self.view)
-//        }
-//
-//    }
+    //    var initialCenter = CGPoint()
+    //    var movedImage = UIImageView ()
+    //    var dropArea = UIView ()
+    //
+    //    @IBAction func dragDrop(_ sender: Any) {
+    //
+    //        func drag (sender: UIPanGestureRecognizer) {
+    //
+    //            //               self.view.bringSubviewToFront(semente)
+    //
+    //            let translation = sender.translation(in: self.view)
+    //
+    //            //               if let view = sender.view {
+    //            //                   view.center = CGPoint(x:view.center.x + translation.x,
+    //            //                                         y:view.center.y + translation.y)
+    //
+    //            //                   if sender.state == UIGestureRecognizer.State.ended {
+    //
+    //            let here = sender.location(in: self.view)
+    //            let newCenter = CGPoint(x: initialCenter.x + translation.x, y: initialCenter.y + translation.y)
+    //
+    //            if (viewMaracuja.frame.contains(here)) {
+    //                semente.center = newCenter }
+    //
+    //            else {
+    //                semente.center = initialCenter
+    //                //                       }
+    //
+    //            }
+    //
+    //            sender.setTranslation(CGPoint.zero, in: self.view)
+    //        }
+    //
+    //    }
     
     
     
@@ -130,21 +108,45 @@ class MaracujaViewController: UIViewController, UIGestureRecognizerDelegate {
         
         viewMaracuja.backgroundColor = .clear
         
-        UIView.animate(withDuration: 1, delay: 0.0, options:[UIView.AnimationOptions.repeat, UIView.AnimationOptions.autoreverse], animations: {
+        UIView.animate(withDuration: 1, delay: 0.0, options:[.allowUserInteraction, UIView.AnimationOptions.repeat, UIView.AnimationOptions.autoreverse], animations: {
             self.viewCores.backgroundColor = .red
             self.viewCores.backgroundColor = .purple
         }, completion: nil)
         
         
-        
+        let tapGesture = UITapGestureRecognizer(target: self, action: #selector(tapped(_:)))
+        self.view.addGestureRecognizer(tapGesture)
         
     } // fecha viewDidLoad
     
+    @objc func tapped(_ recognizer:UITapGestureRecognizer) {
+        
+//        guard let location = recognizer.location(in: self.view) else {return}
+        
+        if viewMaracuja.frame.contains(recognizer.location(in: self.view)) {
+            buttonProximo.isHidden = false
+            imageMaracuja.image = maracuja
+            semente.isHidden = false
+            
+        } else {
+            let animation = CABasicAnimation(keyPath: "position")
+                       animation.duration = 0.07
+                       animation.repeatCount = 4
+                       animation.autoreverses = true
+                       animation.fromValue = NSValue(cgPoint: CGPoint(x: viewMaracuja.center.x - 8, y: viewMaracuja.center.y))
+                       animation.toValue = NSValue(cgPoint: CGPoint(x: viewMaracuja.center.x + 8, y: viewMaracuja.center.y))
+                       
+                       viewMaracuja.layer.add(animation, forKey: "position")
+        }
+        
+    } // fecha tapped
     
+
 } // fecha view controller
 
 
 
 
 
+//     tocou fora do maracuja:
 
